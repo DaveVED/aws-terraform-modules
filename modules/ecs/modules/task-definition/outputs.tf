546bf5949205d5b99c6a97f3d269fd007a57fed2
DaveVED <dave.denns@gs.com>
2023-08-15 17:50:42 -0500

    feat: new output

--- modules/ecs/modules/task-definition/outputs.tf
+++ modules/ecs/modules/task-definition/outputs.tf
@@ -7,3 +7,14 @@ output "task_definitions" {
   description = "List of the ECS Task Definitions"
   value       = values(aws_ecs_task_definition.ecs)
 }
+
+output "task_definitions_info" {
+  description = "A list of task definitions with their ARN, family, and port."
+  value = [
+    for td in aws_ecs_task_definition.ecs : {
+      arn    = td.arn
+      family = td.family
+      port   = try(td.container_definitions[0].portMappings[0].containerPort, null)  # Assumes only one container definition per task
+    }
+  ]
+}
