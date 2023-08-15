02d9715fcc72cf4360791b28e391f932dba4308d
DaveVED <dave.denns@gs.com>
2023-08-15 18:09:41 -0500

    feat: one tg

--- modules/ecs/modules/service/ecs-service.tf
+++ modules/ecs/modules/service/ecs-service.tf
@@ -8,7 +8,7 @@ resource "aws_ecs_service" "ecs_alb" {
   desired_count   = var.desired_task_count
 
   load_balancer {
-    target_group_arn = var.load_balancer_target_group_arns[each.key]
+    target_group_arn = var.load_balancer_target_group_arns
     container_name   = var.container_names[each.key] 
     container_port   = var.container_ports[each.key]
   }
