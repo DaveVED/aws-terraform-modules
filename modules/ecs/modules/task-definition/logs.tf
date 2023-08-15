6ccecc681cab2f2d99efaf7732a763494e522957
DaveVED <dave.denns@gs.com>
2023-08-15 17:02:49 -0500

    feat: fixing a typo

--- modules/ecs/modules/task-definition/logs.tf
+++ modules/ecs/modules/task-definition/logs.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "ecs" {
   for_each = var.task_definitions
 
-  name = "${each.value.task_definition.family}-task-definition-logs"
+  name = "${each.value.family}-task-definition-logs"
 }
