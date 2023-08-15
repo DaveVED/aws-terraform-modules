36515050b444063f3fe8ccd89c8e40ebc304cb41
DaveVED <dave.denns@gs.com>
2023-08-15 18:04:27 -0500

    feat: fixing outputs

--- modules/ecs/modules/alb/outputs.tf
+++ modules/ecs/modules/alb/outputs.tf
@@ -1,4 +1,4 @@
 output "load_balancer_target_group_arns" {
   description = "ARNs of the ALB Target Groups"
-  value       = { for tg in aws_lb_target_group.target_group : "${tg.port}-${tg.health_check.path}" => tg.arn }
+  value = { for tg in aws_lb_target_group.target_group : "${tg.port}-${tg.health_check[0].path}" => tg.arn }
 }
