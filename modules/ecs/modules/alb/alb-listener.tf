d0d5b9aedbc880bd9db925a7652ad2cbce1d45d5
DaveVED <dave.w.dennis@gmail.com>
2023-08-18 02:49:58 -0500

    feat: udpate sll policy

--- modules/ecs/modules/alb/alb-listener.tf
+++ modules/ecs/modules/alb/alb-listener.tf
@@ -2,7 +2,7 @@ resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_alb.application_load_balancer.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = var.cert_arn
 
   # Dynamically set the default actions for each target group
