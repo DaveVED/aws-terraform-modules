14091de28c7625bd634f957f4fbaad531127c302
Dave Dennis <dave.w.dennis@gmail.com>
2023-09-03 13:48:23 -0500

    feat: working on aliasi

--- modules/ecs/modules/cluster/kms.tf
+++ modules/ecs/modules/cluster/kms.tf
@@ -41,6 +41,6 @@ data "aws_iam_policy_document" "kms_policy" {
 }
 
 resource "aws_kms_alias" "alias" {
-  name          = "alias/${var.project}/${var.environment}/ecs"
+  name          = "alias/${var.project}/ecs"
   target_key_id = aws_kms_key.key.arn
 }
